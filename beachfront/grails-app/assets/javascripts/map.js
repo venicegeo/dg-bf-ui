15e434416db780d95c837ac182bd3e50057c4adf
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-03-14 13:57:06 -0400

    Started making room for a layer switcher.

--- beachfront/grails-app/assets/javascripts/map.js
+++ beachfront/grails-app/assets/javascripts/map.js
@@ -1,3 +1,32 @@
+function createLayerSwitcherControl() {
+        var layerSwitcherControl = function(opt_options) {
+
+        var options = opt_options || {};
+
+        var button = document.createElement('button');
+        button.innerHTML = 'N';
+
+        var this_ = this;
+        var handleRotateNorth = function() {
+          this_.getMap().getView().setRotation(0);
+        };
+
+        button.addEventListener('click', handleRotateNorth, false);
+        button.addEventListener('touchstart', handleRotateNorth, false);
+
+        var element = document.createElement('div');
+        element.className = 'rotate-north ol-unselectable ol-control';
+        element.appendChild(button);
+
+        ol.control.Control.call(this, {
+          element: element,
+          target: options.target
+        });
+
+      };
+      ol.inherits(app.RotateNorthControl, ol.control.Control);
+}
+
 function createContextMenuContent(coordinate) {
 	var coordConvert = new CoordinateConversion();
 	var latitude = coordinate[1];
