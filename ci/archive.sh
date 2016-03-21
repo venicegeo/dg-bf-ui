a97c93aa4e55586aebbd43b7ffa1b9a81ec22a23
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-03-21 08:28:46 -0400

    Tesint new deployment

--- ci/archive.sh
+++ ci/archive.sh
@@ -10,6 +10,7 @@ source $root/ci/vars.sh
 ! type grails >/dev/null 2>&1 && source $root/ci/grails.sh
 
 pushd $root/$APP > /dev/null
+pwd
   grails compile
   grails -Dbuild.compiler=javac1.7 build-standalone $root/$APP.$EXT
 popd > /dev/null
