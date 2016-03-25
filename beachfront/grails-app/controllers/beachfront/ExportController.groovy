3e2e64b180d73c6940282bea3db25f52c2763731
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-03-25 08:19:17 -0400

    Fixed screenshot export.

--- beachfront/grails-app/controllers/beachfront/ExportController.groovy
+++ beachfront/grails-app/controllers/beachfront/ExportController.groovy
@@ -11,27 +11,12 @@ class ExportController {
 
 
 	def exportCanvas() {
-
-		//def requestMap
-		//if (!params.imageData) {
-		//requestMap = request.reader.text.split('&').inject([:]) {
-		//	map, token ->
-		//		token.split('=').with { map[it[0]] = java.net.URLDecoder.decode(it[1]) }
-		//		map
-		//	}
-		//println requestMap      } 
-		//else { requestMap = params }         
-                
-
-
-
-
 		def requestMap = restApiService.normalizeRequestParams(params, request)
 		def image = exportService.canvasToImage([imageData: requestMap.imageData])
 
 
 		response.contentType = "image/png"
-		def filename = "BF_Screenshot_${new Date().format("yyyyMMddHHmmssSSS")}"
+		def filename = "BF_Screenshot_${new Date().format("yyyyMMddHHmmssSSS")}.png"
 		response.setHeader("Content-disposition", "attachment;filename=${filename}")
 		ImageIO.write(image, "png", response.outputStream)
 		response.outputStream.flush()
