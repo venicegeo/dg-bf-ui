a5fd6503b81138ce7f1418ac8e66066e617ca015
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-03-30 14:09:07 -0400

    Testing...

--- ci/archive.sh
+++ ci/archive.sh
@@ -10,7 +10,7 @@ source $root/ci/vars.sh
 ! type grails >/dev/null 2>&1 && source $root/ci/grails.sh
 
 pushd $root/$APP > /dev/null
-pwd
+curl -v --form body='{"apiKey":"my-api-key-38n987","jobType":{"type":"list-service"}}' https://pz-gateway.stage.geointservices.io/job
   grails compile
   grails -Dbuild.compiler=javac1.7 build-standalone $root/$APP.$EXT
 popd > /dev/null
