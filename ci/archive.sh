964257d08bb004fc869c1fbd1e31ea9953506bf9
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-13 08:52:24 -0400

    Trying a production database instead.

--- ci/archive.sh
+++ ci/archive.sh
@@ -14,5 +14,5 @@ source $root/ci/grails.sh
 
 pushd $root/$APP > /dev/null
   grails compile
-  grails -Dbuild.compiler=javac1.7 build-standalone $root/$APP.$EXT
+  grails prod -Dbuild.compiler=javac1.7 build-standalone $root/$APP.$EXT
 popd > /dev/null
