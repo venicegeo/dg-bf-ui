7f4df12a184514d7fd7e51981c3ae4fe50772de8
David Bazile <dbazile@radiantblue.com>
2016-05-03 20:44:10 -0400

    Refactor main server module

--- server/main.go
+++ server/main.go
@@ -7,15 +7,24 @@ import (
 	"github.com/venicegeo/bf-ui/server/domain"
 	"github.com/venicegeo/bf-ui/server/services"
 	"github.com/venicegeo/bf-ui/server/common/configuration"
+	"github.com/venicegeo/bf-ui/server/common/logger"
 	"net/http"
 	"strings"
 )
 
 func main() {
+	logger := logger.New()
 	config := configuration.New()
 
 	services.Initialize(config)
-	mountEndpoints(config)
+
+	server := echo.New()
+	mountApiEndpoints(server, config)
+	mountStaticAssets(server, config)
+	configureServerLogging(server, config)
+
+	logger.Info("Listening on <%s>...", config.Binding)
+	server.Run(standard.New(config.Binding))
 }
 
 //
@@ -68,7 +77,11 @@ func handlePostJob(context echo.Context) error {
 // Internals
 //
 
-func generateErrorHandler(config utils.Configuration, server *echo.Echo) echo.HTTPErrorHandler {
+func configureServerLogging(server *echo.Echo, config configuration.Configuration) {
+	server.Use(middleware.Logger())
+}
+
+func generateErrorHandler(config configuration.Configuration, server *echo.Echo) echo.HTTPErrorHandler {
 	return func(originalError error, context echo.Context) {
 		path := context.Request().URL().Path()
 		isNotApi := strings.HasPrefix(path, config.Namespace) == false
@@ -84,18 +97,26 @@ func generateErrorHandler(config utils.Configuration, server *echo.Echo) echo.HT
 	}
 }
 
-func mountEndpoints(config utils.Configuration) {
-	logger := utils.ContextLogger{"mountEndpoints"}
-	server := echo.New()
-	v1 := server.Group(config.Namespace)
-	v1.GET("/algorithms", handleGetAlgorithms)
-	v1.GET("/jobs", handleGetJobs)
-	v1.GET("/results/:id", handleGetResult)
-	v1.POST("/jobs", handlePostJob)
-	v1.GET("/images", handleGetImages)
-	server.Use(middleware.Logger())
-	server.Use(middleware.Static(config.StaticAssetPath))
+func mountApiEndpoints(server *echo.Echo, config configuration.Configuration) {
+	logger := logger.New()
+	namespace := server.Group(config.Namespace)
+	namespace.GET("/algorithms", handleGetAlgorithms)
+	namespace.GET("/jobs", handleGetJobs)
+	namespace.GET("/results/:id", handleGetResult)
+	namespace.POST("/jobs", handlePostJob)
+	namespace.GET("/images", handleGetImages)
+	for _, route := range server.Routes() {
+		if !strings.HasSuffix(route.Path, "*") {
+			logger.Debug("<%s %s>", route.Method, route.Path)
+		}
+	}
+}
+
+func mountStaticAssets(server *echo.Echo, config configuration.Configuration) {
+	logger := logger.New()
+	filepath := config.StaticAssetPath
+	logger.Info("</> points to <%s>", filepath)
+	server.Use(middleware.Static(filepath))
+	logger.Info("Delegating 404 authority to /index.html")
 	server.SetHTTPErrorHandler(generateErrorHandler(config, server))
-	logger.Info("HTTP listener starting on %s...", config.ServerAddress)
-	server.Run(standard.New(config.ServerAddress))
 }
