1ef565372554a9d059795e9490101b7ce88f0aef
David Bazile <dbazile@radiantblue.com>
2016-05-04 09:22:14 -0400

    Fix CI build environment for server

--- ci/build_server.sh
+++ ci/build_server.sh
@@ -4,11 +4,19 @@ function build_server() {
   local root=$1
   local target=$2
 
-  pushd $root/server > /dev/null
+  # Create a temporary GOPATH and copy source files into it
+  export GOPATH=$root/gopath
+  mkdir -p $GOPATH/src/github.com/venicegeo/bf-ui
+  cp -r $root/server $GOPATH/src/github.com/venicegeo/bf-ui
+  pushd $GOPATH/src/github.com/venicegeo/bf-ui/server > /dev/null
 
-  go get -v
-  go test
+  ###
+
+  go get -v -t ./...
+  go test -v ./...
   go build -o $target/beachfront-server
+
+  ###
 
   popd > /dev/null
 }
