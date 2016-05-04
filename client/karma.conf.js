75206b7a51d44ad5b50610ef5bfd6628d992eca9
David Bazile <dbazile@radiantblue.com>
2016-05-03 22:20:37 -0400

    Install karma

---
+++ client/karma.conf.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+
+module.exports = (config) => {
+  config.set({
+    browsers: ['Chrome'],
+    frameworks: ['jasmine'],
+    singleRun: true,
+
+    files: ['app/**/*test.js'],
+    preprocessors: {
+      'app/**/*.js': ['webpack']
+    },
+
+    webpack: {
+      devtool: 'cheap-module-eval-source-map',
+      module: {
+        loaders: [
+          {pattern: /\.jsx?$/, loader: 'babel', exclude: /node_modules/},
+          {test: /\.css$/, loader: 'style!css?module&localIdentName=[name]__[local]'}
+        ]
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('test'),
+          'process.env.API_NAMESPACE': JSON.stringify('/api/test')
+        })
+      ]
+    }
+  })
+}
--- client/test/config-test.js
+++
@@ -1,8 +0,0 @@
-import {SOME_CONFIG_PROPERTY} from '../app/config';
-import assert from 'assert';
-
-describe('application config', () => {
-  it('has a `SOME_CONFIG_PROPERTY` configuration', () => {
-    assert.equal(SOME_CONFIG_PROPERTY, 'lorem ipsum');
-  });
-});
