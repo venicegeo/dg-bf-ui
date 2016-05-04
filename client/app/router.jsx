599adb5b6f93182880444e9c8f2bc882d9d410f0
David Bazile <dbazile@radiantblue.com>
2016-05-04 17:21:01 -0400

    Resolve base-href deprecation warning

--- client/app/router.jsx
+++ client/app/router.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Router, Route, browserHistory} from 'react-router';
 import {render} from 'react-dom';
 import Application from './components/Application';
 import CreateJob from './components/CreateJob';
 import JobStatusList from './components/JobStatusList';
+import {createHistory} from 'history'
+import {Router, Route, useRouterHistory} from 'react-router'
 
 export function bootstrap(element) {
+  const history = useRouterHistory(createHistory)({
+    basename: '/'
+  })
   render(
-    <Router history={browserHistory}>
+    <Router history={history}>
       <Route path="/" component={Application}>
         <Route path="job" component={JobStatusList}/>
         <Route path="job/:resultId" component={JobStatusList}/>
