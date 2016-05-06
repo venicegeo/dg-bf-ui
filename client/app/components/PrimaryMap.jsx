6033b3eb2c772a4d436fdb176002d39451882070
David Bazile <dbazile@radiantblue.com>
2016-05-06 17:12:01 -0400

    Restore ability to change basemaps

--- client/app/components/PrimaryMap.jsx
+++ client/app/components/PrimaryMap.jsx
@@ -4,9 +4,9 @@ import openlayers from 'openlayers'
 import styles from './PrimaryMap.less'
 import {TILE_PROVIDERS} from '../config'
 
-const [DEFAULT_TILE_PROVIDER] = TILE_PROVIDERS
 const INITIAL_CENTER = [-20, 0]
-const INITIAL_ZOOM = 2.5
+const MIN_ZOOM = 2.5
+const MAX_ZOOM = 22
 const DETECTED = 'Detected'
 const UNDETECTED = 'Undetected'
 const NEW_DETECTION = 'New Detection'
@@ -18,31 +18,38 @@ export default class PrimaryMap extends Component {
 
   constructor() {
     super()
+    this._basemaps = []
     this._layers = []
+    this.state = {basemapIndex: 0}
     this._export = this._export.bind(this)
-    this._generateStyles = this._generateStyles.bind(this)
   }
 
   componentDidMount() {
-    this._initializeOpenLayers(DEFAULT_TILE_PROVIDER)
+    this._initializeOpenLayers()
     this._renderLayers()
   }
 
-  componentDidUpdate() {
-    this._clearLayers()
-    this._renderLayers()
+  componentDidUpdate(previousProps) {
+    if (this.props.featureCollections !== previousProps.featureCollections) {
+      this._clearLayers()
+      this._renderLayers()
+    }
+    this._updateBasemap()
   }
 
   render() {
     return (
       <div className={styles.root} ref="container">
-        <div className={styles['export-button']} title="Click to export an image of this map">
+        <div className={styles.download} title="Click to export an image of this map">
           <a ref="downloadButton"
              download="map.png"
              onClick={this._export}>
             <i className="fa fa-download"/>
           </a>
         </div>
+        <ul className={styles.basemaps}>
+          {this._basemaps.map((layer, i) => <li className={this.state.basemapIndex === i ? styles.active : ''} key={i} onClick={() => this.setState({basemapIndex: i})}>{layer.get('name')}</li>)}
+        </ul>
       </div>
     )
   }
@@ -51,16 +58,17 @@ export default class PrimaryMap extends Component {
   // Internals
   //
 
-  _initializeOpenLayers(provider) {
+  _initializeOpenLayers() {
+    this._basemaps = generateBasemapLayers(TILE_PROVIDERS)
     this._map = new openlayers.Map({
-      controls: this._generateControls(),
-      layers: this._generateBasemapLayers(provider),
+      controls: generateControls(),
+      layers: this._basemaps,
       target: this.refs.container,
       view: new openlayers.View({
         center: openlayers.proj.fromLonLat(INITIAL_CENTER),
-        minZoom: INITIAL_ZOOM,
-        maxZoom: provider.maxZoom,
-        zoom: INITIAL_ZOOM
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        zoom: MIN_ZOOM
       })
     })
   }
@@ -79,13 +87,13 @@ export default class PrimaryMap extends Component {
       const features = reader.readFeatures(featureCollection.geojson, {featureProjection:'EPSG:3857'})
       features.forEach(feature => {
         const source = new openlayers.source.Vector({features: [feature]})
-        const layer = new openlayers.layer.Vector({source, style: this._generateStyles})
+        const layer = new openlayers.layer.Vector({source, style: generateStyles})
         openlayers.extent.extend(bounds, source.getExtent())
         openlayers.extent.extend(viewport, source.getExtent())
         this._layers.push(layer)
         this._map.addLayer(layer)
       })
-      const frame = this._generateFrame(bounds, featureCollection.name)
+      const frame = generateFrame(bounds, featureCollection.name)
       this._layers.push(frame)
       this._map.addLayer(frame)
     })
@@ -103,125 +111,130 @@ export default class PrimaryMap extends Component {
     this._map.renderSync()
   }
 
-  _generateBasemapLayers(provider) {
-    return [
-      new openlayers.layer.Tile({
-        source: new openlayers.source.XYZ(Object.assign({crossOrigin: 'anonymous'}, provider))
-      })
-    ];
+  _updateBasemap() {
+    this._basemaps.forEach((layer, i) => layer.setVisible(i === this.state.basemapIndex))
   }
+}
 
-  _generateControls() {
-    return openlayers.control.defaults({
-      attributionOptions: {collapsible: false}
-    }).extend([
-      new openlayers.control.ScaleLine({
-        minWidth: 250,
-        units: 'nautical'
-      }),
-      new openlayers.control.ZoomSlider(),
-      new openlayers.control.MousePosition({
-        coordinateFormat: openlayers.coordinate.toStringHDMS
-      }),
-      new openlayers.control.FullScreen()
-    ])
-  }
+function generateBasemapLayers(providers) {
+  return providers.map((provider, index) => {
+    const source = new openlayers.source.XYZ(Object.assign({crossOrigin: 'anonymous'}, provider))
+    const layer = new openlayers.layer.Tile({source})
+    layer.setProperties({name: provider.name, visible: index === 0})
+    return layer
+  })
+}
 
-  _generateFrame(bounds, title) {
-    return new openlayers.layer.Vector({
-      source: new openlayers.source.Vector({
-        features: [new openlayers.Feature({
-          geometry: openlayers.geom.Polygon.fromExtent(bounds)
-        })]
+function generateControls() {
+  return openlayers.control.defaults({
+    attributionOptions: {collapsible: false}
+  }).extend([
+    new openlayers.control.ScaleLine({
+      minWidth: 250,
+      units: 'nautical'
+    }),
+    new openlayers.control.ZoomSlider(),
+    new openlayers.control.MousePosition({
+      coordinateFormat: openlayers.coordinate.toStringHDMS
+    }),
+    new openlayers.control.FullScreen()
+  ])
+}
+
+function generateFrame(bounds, title) {
+  return new openlayers.layer.Vector({
+    source: new openlayers.source.Vector({
+      features: [new openlayers.Feature({
+        geometry: openlayers.geom.Polygon.fromExtent(bounds)
+      })]
+    }),
+    style: new openlayers.style.Style({
+      stroke: new openlayers.style.Stroke({
+        color: 'rgba(0, 0, 0, .2)',
+        lineDash: [20, 20],
+        weight: 5
       }),
-      style: new openlayers.style.Style({
-        stroke: new openlayers.style.Stroke({
-          color: 'rgba(0, 0, 0, .2)',
-          lineDash: [20, 20],
-          weight: 5
-        }),
-        text: new openlayers.style.Text({
-          text: title,
-          font: 'bold 15px Menlo, monospace',
-          fill: new openlayers.style.Fill({
-            color: 'rgba(0, 0, 0, .2)'
-          })
+      text: new openlayers.style.Text({
+        text: title,
+        font: 'bold 15px Menlo, monospace',
+        fill: new openlayers.style.Fill({
+          color: 'rgba(0, 0, 0, .2)'
         })
       })
     })
-  }
+  })
+}
 
-  _generateStyles(feature) {
-    const geometry = feature.getGeometry()
-    switch (feature.get('detection')) {
-      case DETECTED:
-        const [baseline, detection] = geometry.getGeometries()
-        return [this._generateStyleDetectionBaseline(baseline), this._generateStyleDetection(detection)]
-      case UNDETECTED:
-        return [this._generateStyleUndetected()]
-      case NEW_DETECTION:
-        return [this._generateStyleNewDetection()]
-      default:
-        return [this._generateStyleUnknownDetectionType()]
-    }
+function generateStyles(feature) {
+  const geometry = feature.getGeometry()
+  switch (feature.get('detection')) {
+    case DETECTED:
+      const [baseline, detection] = geometry.getGeometries()
+      return [generateStyleDetectionBaseline(baseline), generateStyleDetection(detection)]
+    case UNDETECTED:
+      return [generateStyleUndetected()]
+    case NEW_DETECTION:
+      return [generateStyleNewDetection()]
+    default:
+      return [generateStyleUnknownDetectionType()]
   }
+}
 
-  _generateStyleDetectionBaseline(baseline) {
-    return new openlayers.style.Style({
-      geometry: baseline,
-      stroke: new openlayers.style.Stroke({
-        color: 'hsla(160, 100%, 30%, .5)',
-        width: 2,
-        lineDash: [5, 5],
-        lineCap: 'miter',
-        lineJoin: 'miter'
-      })
+function generateStyleDetectionBaseline(baseline) {
+  return new openlayers.style.Style({
+    geometry: baseline,
+    stroke: new openlayers.style.Stroke({
+      color: 'hsla(160, 100%, 30%, .5)',
+      width: 2,
+      lineDash: [5, 5],
+      lineCap: 'miter',
+      lineJoin: 'miter'
     })
-  }
+  })
+}
 
-  _generateStyleDetection(detection) {
-    return new openlayers.style.Style({
-      geometry: detection,
-      fill: new openlayers.style.Fill({
-        color: 'hsla(160, 100%, 30%, .2)'
-      }),
-      stroke: new openlayers.style.Stroke({
-        color: 'hsla(160, 100%, 30%, .75)',
-        width: 2
-      })
+function generateStyleDetection(detection) {
+  return new openlayers.style.Style({
+    geometry: detection,
+    fill: new openlayers.style.Fill({
+      color: 'hsla(160, 100%, 30%, .2)'
+    }),
+    stroke: new openlayers.style.Stroke({
+      color: 'hsla(160, 100%, 30%, .75)',
+      width: 2
     })
-  }
+  })
+}
 
-  _generateStyleUndetected() {
-    return new openlayers.style.Style({
-      fill: new openlayers.style.Fill({
-        color: 'hsla(0, 100%, 75%, .2)'
-      }),
-      stroke: new openlayers.style.Stroke({
-        color: 'red',
-        width: 2,
-        lineDash: [5, 5],
-        lineCap: 'miter',
-        lineJoin: 'miter'
-      })
+function generateStyleNewDetection() {
+  return new openlayers.style.Style({
+    stroke: new openlayers.style.Stroke({
+      width: 2,
+      color: 'hsl(205, 100%, 50%)'
     })
-  }
+  })
+}
 
-  _generateStyleNewDetection() {
-    return new openlayers.style.Style({
-      stroke: new openlayers.style.Stroke({
-        width: 2,
-        color: 'hsl(205, 100%, 50%)'
-      })
+function generateStyleUndetected() {
+  return new openlayers.style.Style({
+    fill: new openlayers.style.Fill({
+      color: 'hsla(0, 100%, 75%, .2)'
+    }),
+    stroke: new openlayers.style.Stroke({
+      color: 'red',
+      width: 2,
+      lineDash: [5, 5],
+      lineCap: 'miter',
+      lineJoin: 'miter'
     })
-  }
+  })
+}
 
-  _generateStyleUnknownDetectionType() {
-    return new openlayers.style.Style({
-      stroke: new openlayers.style.Stroke({
-        color: 'magenta',
-        width: 2
-      })
+function generateStyleUnknownDetectionType() {
+  return new openlayers.style.Style({
+    stroke: new openlayers.style.Stroke({
+      color: 'magenta',
+      width: 2
     })
-  }
+  })
 }
