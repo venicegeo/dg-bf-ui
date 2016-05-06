8816fd86fe724db397103f11495fe3942f0ff3e4
David Bazile <dbazile@radiantblue.com>
2016-05-06 17:11:29 -0400

    Fix aerial tile provider attribution

--- client/app/config.js
+++ client/app/config.js
@@ -16,7 +16,7 @@ export const TILE_PROVIDERS = [
   {
     name: 'Aerial',
     url: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png64?access_token=pk.eyJ1IjoiYmF6aWxlcmJ0IiwiYSI6ImNpbmpwcjlrMzB4cHN0dG0zdDJpMWV6ZjkifQ.7Vywvn-z3L6nfSeI4v-Rdg',
-    attributions: '&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank" rel="noopener">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/about/" target="_blank" rel="noopener">OpenStreetMap</a>',
+    attributions: '&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank" rel="noopener">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/about/" target="_blank" rel="noopener">OpenStreetMap</a> &copy; <a href="https://www.digitalglobe.com/" target="_blank" rel="noopener">DigitalGlobe</a>',
     maxZoom: 22
   },
   {
