f364cb767b4c1d4e30e567df88a33ac5d4765b0a
David Bazile <dbazile@radiantblue.com>
2016-05-18 16:52:28 -0400

    Clean up

--- client/app/api/piazza-client.js
+++ client/app/api/piazza-client.js
@@ -1,10 +1,10 @@
 export const STATUS_RUNNING = 'Running'
 export const STATUS_SUCCESS = 'Success'
-export const STATUS_ERROR = 'Error'
+export const STATUS_ERROR   = 'Error'
 
 export class Client {
   constructor(gateway, authToken) {
-    this.gateway = gateway.replace(/\/+$/g, '')
+    this.gateway   = gateway.replace(/\/+$/g, '')
     this.authToken = authToken
   }
 
@@ -16,38 +16,23 @@ export class Client {
   getServices({pattern}) {
     return this._fetch(`/service?keyword=${pattern}&per_page=100`)
       .then(asJson)
-      .then(page => page.data)
+      .then(normalizeServiceListing)
   }
-  
+
   getStatus(jobId) {
     return this._fetch(`/job/${jobId}`)
       .then(asJson)
-      .then(status => {
-        if (!status.status || status.type === 'error') {
-          throw new InvalidResponse(status, status.message || 'Status is ambiguous')
-        }
-        return Object.assign({
-          message: null,
-          result: null
-        }, status)
-      })
+      .then(normalizeStatus)
   }
 
   post(type, data) {
     return this._fetch('/v2/job', {
-      method: 'POST',
       body: JSON.stringify({type, data}),
-      headers: {
-        'content-type': 'application/json'
-      }
+      headers: {'content-type': 'application/json'},
+      method: 'POST'
     })
       .then(asJson)
-      .then(metadata => {
-        if (!metadata.jobId) {
-          throw new InvalidResponse(metadata, 'No job ID assigned')
-        }
-        return metadata.jobId
-      })
+      .then(normalizePostMetadata)
   }
 
   _fetch(endpoint, overrides = {}) {
@@ -61,7 +46,7 @@ export class Client {
 }
 
 //
-// Helpers
+// Internals
 //
 
 function asJson(response) {
@@ -76,6 +61,27 @@ function asText(response) {
     throw new HttpError(response)
   }
   return response.text()
+}
+
+function normalizePostMetadata(metadata) {
+  if (!metadata.jobId) {
+    throw new InvalidResponse(metadata, 'No job ID assigned')
+  }
+  return metadata.jobId
+}
+
+function normalizeServiceListing(page) {
+  return page.data
+}
+
+function normalizeStatus(status) {
+  if (!status.status || status.type === 'error') {
+    throw new InvalidResponse(status, status.message || 'Status is ambiguous')
+  }
+  return Object.assign({
+    message: null,
+    result: null
+  }, status)
 }
 
 //
