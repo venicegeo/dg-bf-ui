3a4b3e67b02726f5b5e10711ee4fab899fae0c77
Jen Duncan <jduncan@radiantblue.com>
2016-05-18 09:33:33 -0400

    first round of commits for the login feature.  No where close to working yet.  For some reason the login thinks the user is logged in when the site first comes up.  ???

--- client/app/components/Navigation.jsx
+++ client/app/components/Navigation.jsx
@@ -1,12 +1,34 @@
 import styles from './Navigation.less'
 import React, {Component} from 'react'
 import Link from 'react-router/lib/Link'
+import auth from '../utils/auth'
 
 export default class Navigation extends Component {
   static propTypes = {
     currentLocation: React.PropTypes.object
   }
-  
+
+  constructor () {
+    super();
+    this.state = {
+      loggedIn: null,
+    };
+    this.updateAuth = this.updateAuth.bind(this);
+  }
+
+
+
+  updateAuth(loggedIn) {
+
+    this.setState({
+      loggedIn: !!loggedIn
+    })
+  }
+
+  componentWillMount() {
+    auth.onChange = this.updateAuth
+    auth.login()
+  }
   render() {
     const {pathname} = this.props.currentLocation
     return (
--- client/app/router.jsx
+++ client/app/router.jsx
@@ -3,8 +3,25 @@ import {createHistory} from 'history'
 import {Router, Route, useRouterHistory} from 'react-router'
 import {render} from 'react-dom'
 import Application from './components/Application'
+import Login from './components/Login'
 import CreateJob from './components/CreateJob'
 import JobStatusList from './components/JobStatusList'
+import auth from './utils/auth.js'
+
+function redirectToLogin(nextState, replace) {
+  if (!auth.loggedIn()) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname }
+    })
+  }
+}
+
+function redirectToDashboard(nextState, replace) {
+  if (auth.loggedIn()) {
+    replace('/')
+  }
+}
 
 export function bootstrap(element) {
   const history = useRouterHistory(createHistory)({
@@ -13,10 +30,11 @@ export function bootstrap(element) {
   render(
     <Router history={history}>
       <Route path="/" component={Application}>
-        <Route path="job" component={JobStatusList}/>
-        <Route path="job/:resultId" component={JobStatusList}/>
-        <Route path="new" component={CreateJob}/>
-        <Route path="new/:algorithmId" component={CreateJob}/>
+        <Route path="login" component={Login}/>
+        <Route path="job" component={JobStatusList} onEnter={redirectToLogin}/>
+        <Route path="job/:resultId" component={JobStatusList} onEnter={redirectToLogin}/>
+        <Route path="new" component={CreateJob} onEnter={redirectToLogin}/>
+        <Route path="new/:algorithmId" component={CreateJob} onEnter={redirectToLogin}/>
       </Route>
     </Router>, element)
 }
