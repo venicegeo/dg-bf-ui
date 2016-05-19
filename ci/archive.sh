6c98fd397ed68abea3468dad3e85604ae8c35503
David Bazile <dbazile@radiantblue.com>
2016-05-19 17:47:37 -0400

    Remove server component

--- ci/archive.sh
+++ ci/archive.sh
@@ -4,27 +4,9 @@ pushd `dirname $0`/.. > /dev/null
 root=$(pwd -P)
 popd > /dev/null
 
-#
-# Define Variables
-#
-
 source $root/ci/vars.sh
-build_target=$root/$APP
 
-#
-# Perform Build
-#
-
-mkdir $build_target
-
-source $root/ci/build_client.sh
-source $root/ci/build_server.sh
-
-build_server $root $build_target
-build_client $root $build_target
-
-#
-# Bundle Artifacts
-#
-
-zip -mr ${APP}.${EXT} $APP
+npm install
+npm run test:ci
+npm run build
+# NODE_ENV=production npm run build
