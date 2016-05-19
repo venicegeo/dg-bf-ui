b3e9dc18d599bd27ab2fdf679fc9522c31a64ebf
David Bazile <dbazile@radiantblue.com>
2016-05-19 19:53:36 -0400

    CI: Make archive.sh _actually_ archive

--- ci/archive.sh
+++ ci/archive.sh
@@ -6,8 +6,17 @@ popd > /dev/null
 
 source $root/ci/vars.sh
 
+#
+# Build
+#
+
 npm install
 npm run test:ci
 NODE_ENV=production GATEWAY="https://pz-gateway.${PCF_DOMAIN}" npm run build
-
 echo "pushstate: enabled" > dist/Staticfile
+
+#
+# Package artifacts
+#
+
+zip -jr ${APP}.${EXT} dist
