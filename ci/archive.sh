f45b4bcd3789c0c088b9054b0cb0e85bc4881805
David Bazile <dbazile@radiantblue.com>
2016-05-20 11:28:17 -0400

    CI: Try to get Firefox launched (experiment 4)
    Can firefox even be executed?

--- ci/archive.sh
+++ ci/archive.sh
@@ -10,6 +10,18 @@ source $root/ci/vars.sh
 # Build
 #
 
+
+## FIXME
+## FIXME
+## FIXME
+echo "Attempting to launch Firefox"
+(firefox & echo "pid $!"; sleep 10; echo "killing $!"; kill -9 $!)
+exit 1
+## FIXME
+## FIXME
+## FIXME
+
+
 npm install
 npm run test:ci
 NODE_ENV=production GATEWAY="https://pz-gateway.${PCF_DOMAIN}" npm run build
