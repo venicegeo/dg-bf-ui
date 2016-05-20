2cf3a2f69d6f7db58bce1ed552b00718d2a8d0a7
David Bazile <dbazile@radiantblue.com>
2016-05-20 10:13:17 -0400

    CI: Try to get Firefox launched (experiment 1)

--- ci/archive.sh
+++ ci/archive.sh
@@ -12,7 +12,7 @@ source $root/ci/vars.sh
 
 npm install
 npm run test:ci
-NODE_ENV=production GATEWAY="https://pz-gateway.${PCF_DOMAIN}" npm run build
+FIREFOX_BIN=$(which firefox) NODE_ENV=production GATEWAY="https://pz-gateway.${PCF_DOMAIN}" npm run build
 echo "pushstate: enabled" > dist/Staticfile
 
 #
