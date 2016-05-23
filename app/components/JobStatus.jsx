e3bd72c366e0887b3512f0625e89374eaef4c3ba
David Bazile <dbazile@radiantblue.com>
2016-05-23 15:20:11 -0400

    Add visual indication of which job is being viewed

--- app/components/JobStatus.jsx
+++ app/components/JobStatus.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router'
 import styles from './JobStatus.css'
 import Timer from './Timestamp.jsx'
 
+const STATUS_SUCCESS = 'Success'
 const STATUS_RUNNING = 'Running'
 
 export default class JobStatus extends Component {
@@ -19,20 +20,23 @@ export default class JobStatus extends Component {
     const {job} = this.props
     // TODO -- need to rethink download re: auth
     return (
-      <li className={`${styles.root} ${job.status}`}>
-        <h2>{job.name}</h2>
+      <li className={styles.root}>
+        <Link to={`/jobs/${job.id}`} activeClassName={styles.active} className={job.status}>
+          <h2>{job.name}</h2>
 
-        <div className={styles.details}>
-          <span className={styles.status}>{job.status}</span>
-          {job.status === STATUS_RUNNING && <Timer className={styles.timer} timestamp={job.createdOn}/>}
-        </div>
-
-        {job.status === 'Success' && (
-          <div className={styles.controls}>
-            <Link to={`/jobs/${job.resultId}`}><i className="fa fa-globe"/> View</Link>
-            <a download={`${job.name}.geojson`} href={job.geojsonUrl}><i className="fa fa-cloud-download"/> Download</a>
+          <div className={styles.details}>
+            <span className={styles.status}>{job.status}</span>
+            {job.status === STATUS_RUNNING && <Timer className={styles.timer} timestamp={job.createdOn}/>}
           </div>
-        )}
+
+          {job.status === STATUS_SUCCESS && (
+            <div className={styles.controls}>
+              <a className={styles.button} download={`${job.name}.geojson`} href={job.geojsonUrl}>
+                <i className="fa fa-cloud-download"/> Download
+              </a>
+            </div>
+          )}
+        </Link>
       </li>
     )
   }
