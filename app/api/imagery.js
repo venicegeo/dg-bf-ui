522f6f3843690036857c739bd848ef1c398df598
David Bazile <dbazile@radiantblue.com>
2016-05-24 17:24:47 -0400

    Update canned image ID

--- app/api/imagery.js
+++ app/api/imagery.js
@@ -1,7 +1,6 @@
 export function list(/*client*/) {
   return Promise.resolve([
-    new ImageComposite({name: 'LC80090472014280LGN00', ids: ['e32b37c5-dc6e-4c7b-b11e-81a23ebdbb0a', '2aab2e30-a612-4c5a-8351-324e57a9e993']}),
-    new ImageComposite({name: 'LC80150442014002LGN00', ids: ['65bff6c1-62f4-41d6-a28b-aebb6cb56621', '9c1dc643-5ddd-4f9e-8a3b-6fbc93906a50']})
+    new ImageComposite({name: 'CANNED_IMAGE_1', ids: ['1495f1e3-bb7d-43de-8a9f-629f7861e3f8', '905b82e7-c757-4283-b105-9ce7c8f894de']})
   ])
 }
 
