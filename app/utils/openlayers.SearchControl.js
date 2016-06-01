a93c9d99bdf520f09d28bfed8c05329a57865604
Jen Duncan <jduncan@radiantblue.com>
2016-06-01 08:23:37 -0400

    jump to coordinates not working.  Map will zoom and shift, but then the page seems to refresh and map is reset.  Not sure why this is happening.

--- app/utils/openlayers.SearchControl.js
+++ app/utils/openlayers.SearchControl.js
@@ -1,4 +1,6 @@
 import openlayers from 'openlayers'
+import React from 'react'
+
 
 export default class SearchControl extends openlayers.control.Control {
   constructor(className) {
@@ -6,11 +8,37 @@ export default class SearchControl extends openlayers.control.Control {
     super({element})
     element.className = `${className || ''} ol-unselectable ol-control`
     element.title = 'Jump to a specific location or coordinate'
-    element.innerHTML = '<button><i class="fa fa-search"/></button>'
-    element.addEventListener('click', () => this._clicked())
+    element.innerHTML = '<button><a href="/searchMap"><i class="fa fa-search"/></a></button>'
+    element.addEventListener('click', () => this._searchClicked())
+
   }
 
-  _clicked() {
-    console.debug('things')
-  }
+
+_searchClicked()
+{
+  //open up modal dialog
+  console.log("this")
+  //on submit of the modal dialog, come back here and zoom to location on the map
+  //const map = this.getMap()
+  var long = '38.9072'
+  var lat = '77.0369'
+
+  var ol = require('openlayers');
+  var map = this.getMap()
+  console.log("Long: " + long + " Lat: " + lat);
+  //var olCoordinates = ol.proj.transform([long, lat],"WGS84", "EPSG:900913")
+  //map.getView().setCenter(olCoordinates);
+  //map.getView().setZoom(10);
+  //var extent = [long, lat];
+  //extent = ol.extent.applyTransform(extent, ol.proj.getTransform("EPSG:4326", "EPSG:3857"));
+
+  //map.getView().setCenter(extent)
+ //map.getView().setZoom(1)
+  var view = map.getView()
+  //view.setCenter([long, lat])
+  view.setCenter(ol.proj.transform([long, lat], "EPSG:4326", "EPSG:3857"))
+  view.setZoom(5)
+
+
+}
 }
