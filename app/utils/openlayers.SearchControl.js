0c9f11e42bab44d932ab01460631e26e67fed898
jduncan <jduncan@radiantblue.com>
2016-06-06 12:24:05 -0400

    have a horrendous prompt popping up asking for coords.  User types in lat/lon and values are parsed out.  map centers and zooms to coords.  Need to figure out how to get a prettier solution and beef up the algorithm to parse out values in an input.

--- app/utils/openlayers.SearchControl.js
+++ app/utils/openlayers.SearchControl.js
@@ -4,45 +4,45 @@ import React from 'react'
 
 export default class SearchControl extends openlayers.control.Control {
   constructor(className) {
+
+    var dialog = document.createElement('coordinateDialog')
+    dialog.id = 'coordinateDialog'
+    dialog.innerHTML = '<div id="coordinateDialog" class="test" style="display: none;position: fixed;z-index: 1;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);"><div style="background-color: #fefefe;margin: 15% auto;padding: 20px;border: 1px solid #888; width: 80%;"><span class="close">x</span><p>Some text in the Modal..</p></div></div>'
+    //dialog.style.display = 'block';
+
     const element = document.createElement('div')
     super({element})
     element.className = `${className || ''} ol-unselectable ol-control`
     element.title = 'Jump to a specific location or coordinate'
+    element.id = 'searchCoordBtn'
     element.innerHTML = '<button><i class="fa fa-search"/></button>'
-    element.addEventListener('click', () => this._searchClicked())
+    element.addEventListener('click', () => this._searchClicked(dialog))
 
 
-    const dialog = document.createElement('div')
-    dialog.id = 'coordinateDialog'
-    dialog.innerHTML = '<dialog>This is a dialog</dialog>'
   }
 
 
-  _searchClicked() {
+  _searchClicked(obj) {
 
-
-
-    document.getElementById('coordinateDialog').showModal()
-  //open up modal dialog
-
-
+    //obj.style.display = 'block'
+    var person = prompt("Please enter coordinates");
+    if (person != null) {
+      var res = person.split(" ");
+    }
     console.log('this')
   //on submit of the modal dialog, come back here and zoom to location on the map
-  //const map = this.getMap()
-  //23.7002° S, 133.8806° E
-    var lat = 38.9072
-    var long = -77.0369
 
+    //WASH DC
+    //38.9072 -77.0369
+
+    var lat = parseFloat(res[0])
+    var long = parseFloat(res[1])
     var ol = require('openlayers')
     var map = this.getMap()
     console.log('Long: ' + long + ' Lat: ' + lat)
-
-
     var view = map.getView()
-  //view.setCenter([long, lat])
     view.setCenter(ol.proj.transform([long, lat], 'EPSG:4326', 'EPSG:3857'))
-    view.setZoom(5)
-
+    view.setZoom(8)
 
   }
 }
