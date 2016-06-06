20af686da5b1983119d57f4ee17e69e0eae6e2d9
jduncan <jduncan@radiantblue.com>
2016-06-06 08:48:29 -0400

    jump to coordinates not working.  Map will zoom and shift, but then the page seems to refresh and map is reset.  Not sure why this is happening.

--- app/utils/openlayers.SearchControl.js
+++ app/utils/openlayers.SearchControl.js
@@ -8,37 +8,41 @@ export default class SearchControl extends openlayers.control.Control {
     super({element})
     element.className = `${className || ''} ol-unselectable ol-control`
     element.title = 'Jump to a specific location or coordinate'
-    element.innerHTML = '<button><a href="/searchMap"><i class="fa fa-search"/></a></button>'
+    element.innerHTML = '<button><i class="fa fa-search"/></button>'
     element.addEventListener('click', () => this._searchClicked())
 
+
+    const dialog = document.createElement('div')
+    dialog.id = 'coordinateDialog'
+    dialog.innerHTML = '<dialog>This is a dialog</dialog>'
   }
 
 
-_searchClicked()
-{
+  _searchClicked() {
+
+
+
+    document.getElementById('coordinateDialog').showModal()
   //open up modal dialog
-  console.log("this")
+
+
+    console.log('this')
   //on submit of the modal dialog, come back here and zoom to location on the map
   //const map = this.getMap()
-  var long = '38.9072'
-  var lat = '77.0369'
+  //23.7002° S, 133.8806° E
+    var lat = 38.9072
+    var long = -77.0369
 
-  var ol = require('openlayers');
-  var map = this.getMap()
-  console.log("Long: " + long + " Lat: " + lat);
-  //var olCoordinates = ol.proj.transform([long, lat],"WGS84", "EPSG:900913")
-  //map.getView().setCenter(olCoordinates);
-  //map.getView().setZoom(10);
-  //var extent = [long, lat];
-  //extent = ol.extent.applyTransform(extent, ol.proj.getTransform("EPSG:4326", "EPSG:3857"));
+    var ol = require('openlayers')
+    var map = this.getMap()
+    console.log('Long: ' + long + ' Lat: ' + lat)
 
-  //map.getView().setCenter(extent)
- //map.getView().setZoom(1)
-  var view = map.getView()
+
+    var view = map.getView()
   //view.setCenter([long, lat])
-  view.setCenter(ol.proj.transform([long, lat], "EPSG:4326", "EPSG:3857"))
-  view.setZoom(5)
+    view.setCenter(ol.proj.transform([long, lat], 'EPSG:4326', 'EPSG:3857'))
+    view.setZoom(5)
 
 
-}
+  }
 }
