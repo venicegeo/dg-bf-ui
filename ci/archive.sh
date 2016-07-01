d8fb60000c6bc718ef36f6dc7bec2c7f556b89e9
David Bazile <davidbazile@gmail.com>
2016-07-01 10:15:29 -0400

    Point at a CI-configurable Piazza gateway

--- ci/archive.sh
+++ ci/archive.sh
@@ -12,7 +12,7 @@ source $root/ci/vars.sh
 
 npm install
 xvfb-run npm run test:ci
-NODE_ENV=production GATEWAY="https://pz-gateway.${PCF_DOMAIN}" npm run build
+NODE_ENV=production GATEWAY="${PZ_GATEWAY_URL}" npm run build
 echo "pushstate: enabled" > dist/Staticfile
 
 #
