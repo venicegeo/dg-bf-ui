6b5090caaefe1e11f860cae1d51bd808831feff1
David Bazile <dbazile@radiantblue.com>
2016-08-23 12:49:28 -0400

    Clean up Piazza client test

--- test/utils/piazza-client.test.js
+++ test/utils/piazza-client.test.js
@@ -52,40 +52,35 @@ describe('Piazza Client', function () {
   })
 
   describe('getDeployment()', () => {
-    it('calls correct URL', (done) => {
+    it('calls correct URL', () => {
       const stub = expect.spyOn(window, 'fetch').andReturn(resolveJson(RESPONSE_DEPLOYMENT))
       const client = new Client('http://m', 'test-auth-token')
-      client.getDeployment('test-deployment-id')
+      return client.getDeployment('test-deployment-id')
         .then(() => {
           expect(stub.calls[0].arguments[0]).toEqual('http://m/deployment/test-deployment-id')
-          done()
         })
-        .catch(done)
     })
 
-    it('properly deserializes GeoServer deployment descriptor', (done) => {
+    it('properly deserializes GeoServer deployment descriptor', () => {
       expect.spyOn(window, 'fetch').andReturn(resolveJson(RESPONSE_DEPLOYMENT))
       const client = new Client('http://m', 'test-auth-token')
-      client.getDeployment('test-deployment-id')
+      return client.getDeployment('test-deployment-id')
         .then(descriptor => {
           expect(descriptor.dataId).toEqual('test-data-id')
           expect(descriptor.endpoint).toEqual('http://test-capabilities-url/arbitrary/context/path')
           expect(descriptor.layerId).toEqual('test-layer-id')
-          done()
         })
-        .catch(done)
     })
 
-    it('handles HTTP errors gracefully', (done) => {
+    it('handles HTTP errors gracefully', () => {
       expect.spyOn(window, 'fetch').andReturn(resolveJson(RESPONSE_DEPLOYMENT_NOT_FOUND, 500))
       const client = new Client('http://m', 'test-auth-token')
-      client.getDeployment('nopenope')
-        .then(() => done(new Error('Should have thrown')))
-        .catch(err => {
-          expect(err.status).toEqual(500)
-          done()
-        })
-        .catch(done)
+      return client.getDeployment('nopenope')
+        .then(
+          () => { throw new Error('Should have thrown') },
+          err => {
+            expect(err.status).toEqual(500)
+          })
     })
   })
 
@@ -95,41 +90,35 @@ describe('Piazza Client', function () {
     beforeEach(() => server = sinon.fakeServer.create({autoRespond: true}))
     afterEach(() => server.restore())
 
-    it('calls correct URL', (done) => {
+    it('calls correct URL', () => {
       server.respondWith([200, {}, RESPONSE_FILE])
       const client = new Client('http://m', 'test-auth-token')
-      client.getFile('test-id')
+      return client.getFile('test-id')
         .then(() => {
           expect(server.requests[0].method).toEqual('GET')
           expect(server.requests[0].url).toEqual('http://m/file/test-id')
-          done()
         })
-        .catch(done)
     })
 
-    it('can retrieve file', (done) => {
+    it('can retrieve file', () => {
       server.respondWith([200, {}, RESPONSE_FILE])
       const client = new Client('http://m', 'test-auth-token')
-      client.getFile('test-id')
+      return client.getFile('test-id')
         .then(content => {
           expect(content).toBeTruthy()
-          done()
         })
-        .catch(done)
     })
 
-    it('does not modify payload', (done) => {
+    it('does not modify payload', () => {
       server.respondWith([200, {}, RESPONSE_FILE])
       const client = new Client('http://m', 'test-auth-token')
-      client.getFile('test-id')
+      return client.getFile('test-id')
         .then(actual => {
           expect(actual).toEqual(RESPONSE_FILE)
-          done()
         })
-        .catch(done)
     })
 
-    it.skip('handles HTTP errors gracefully', (done) => {
+    it.skip('handles HTTP errors gracefully', () => {
       /*
        Disabling this test due to a bug in the current version of sinon that causes the
        fake XHR to emit an error event on any non-200 HTTP status code.
@@ -138,224 +127,193 @@ describe('Piazza Client', function () {
        */
       server.respondWith([500, {}, ERROR_GENERIC])
       const client = new Client('http://m', 'test-auth-token')
-      client.getFile('test-id')
-        .then(() => done(new Error('Should have thrown')))
+      return client.getFile('test-id')
+        .then(() => { throw new Error('Should have thrown') })
         .catch(error => {
           expect(error.status).toEqual(500)
-          done()
         })
-        .catch(done)
     })
 
-    it('notifies callers of progress', (done) => {
+    it('notifies callers of progress', () => {
       server.respondWith([200, {'content-length': RESPONSE_FILE.length}, RESPONSE_FILE])
       const client = new Client('http://m', 'test-auth-token')
       const stub = sinon.stub()
-      client.getFile('test-id', stub)
+      return client.getFile('test-id', stub)
         .then(() => {
           expect(stub.called).toBeTruthy()
           expect(stub.alwaysCalledWithMatch(sinon.match({
             loaded: sinon.match.number,
             total: sinon.match.number,
           }))).toBeTruthy()
-          done()
         })
-        .catch(done)
     })
 
-    it('allows callers to cancel a retrieval', (done) => {
+    it('allows callers to cancel a retrieval', () => {
       server.respondWith([200, {}, RESPONSE_FILE])
       const stub = sinon.stub()
       const client = new Client('http://m', 'test-auth-token')
-      client.getFile('test-id', stub)
+      return client.getFile('test-id', stub)
         .then(() => {
           expect(stub.alwaysCalledWithMatch({
             cancel: sinon.match.func
           })).toBeTruthy()
-          done()
         })
-        .catch(done)
     })
 
-    it('on cancellation, rejects promise', (done) => {
+    it('on cancellation, rejects promise', () => {
       server.respondWith([200, {}, RESPONSE_FILE])
       const onProgress = ({cancel}) => cancel()
       const client = new Client('http://m', 'test-auth-token')
-      client.getFile('test-id', onProgress)
-        .then(() => done(new Error('Should have rejected')))
+      return client.getFile('test-id', onProgress)
+        .then(() => { throw new Error('Should have rejected') })
         .catch(err => {
           expect(err).toEqual({isCancellation: true})
-          done()
         })
-        .catch(done)
     })
 
-    it('on cancellation, halts the XHR', (done) => {
+    it('on cancellation, halts the XHR', () => {
       server.respondWith([200, {}, RESPONSE_FILE])
       const onProgress = ({cancel}) => cancel()
       const client = new Client('http://m', 'test-auth-token')
       client.getFile('test-id', onProgress)
         .catch(() => {
           expect(server.requests[0].aborted).toBe(true)
-          done()
         })
-        .catch(done)
     })
   })
 
   describe('getServices()', () => {
-    it('calls correct URL', (done) => {
+    it('calls correct URL', () => {
       const stub = expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_SERVICE_LIST))
       const client = new Client('http://m', 'test-auth-token')
-      client.getServices({pattern: 'test-pattern'})
+      return client.getServices({pattern: 'test-pattern'})
         .then(() => {
           expect(stub.calls[0].arguments[0]).toEqual('http://m/service?keyword=test-pattern&per_page=100')
-          done()
         })
-        .catch(done)
     })
 
-    it('can list services', (done) => {
+    it('can list services', () => {
       expect.spyOn(window, 'fetch').andReturn(resolveJson(RESPONSE_SERVICE_LIST))
       const client = new Client('http://m', 'test-auth-token')
-      client.getServices({pattern: 'test-pattern'})
+      return client.getServices({pattern: 'test-pattern'})
         .then(services => {
           expect(services instanceof Array).toEqual(true)
           expect(services.length).toEqual(2)
-          done()
         })
-        .catch(done)
     })
 
-    it('deserializes metadata', (done) => {
+    it('deserializes metadata', () => {
       expect.spyOn(window, 'fetch').andReturn(resolveJson(RESPONSE_SERVICE_LIST))
       const client = new Client('http://m', 'test-auth-token')
-      client.getServices({pattern: 'test-pattern'})
+      return client.getServices({pattern: 'test-pattern'})
         .then(([firstService]) => {
           expect(firstService.serviceId).toEqual('test-id-1')
           expect(firstService.resourceMetadata.classType).toEqual({classification: 'UNCLASSIFIED'})
           expect(firstService.resourceMetadata.description).toEqual('test-description')
           expect(firstService.resourceMetadata.name).toEqual('test-name')
           expect(firstService.resourceMetadata.version).toEqual('test-version')
-          done()
         })
-        .catch(done)
     })
 
-    it('handles HTTP errors gracefully', (done) => {
+    it('handles HTTP errors gracefully', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(ERROR_GENERIC, 500))
       const client = new Client('http://m', 'test-auth-token')
-      client.getServices({pattern: 'test-pattern'})
-        .then(() => done(new Error('Should have thrown')))
-        .catch(error => {
-          expect(error.status).toEqual(500)
-          done()
-        })
-        .catch(done)
+      return client.getServices({pattern: 'test-pattern'})
+        .then(
+          () => { throw new Error('Should have thrown') },
+          error => {
+            expect(error.status).toEqual(500)
+          })
     })
   })
 
   describe('getStatus()', () => {
-    it('calls correct URL', (done) => {
+    it('calls correct URL', () => {
       const stub = expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_RUNNING))
       const client = new Client('http://m', 'test-auth-token')
-      client.getStatus('test-id')
+      return client.getStatus('test-id')
         .then(() => {
           expect(stub.calls[0].arguments[0]).toEqual('http://m/job/test-id')
-          done()
         })
-        .catch(done)
     })
 
-    it('properly deserializes running job', (done) => {
+    it('properly deserializes running job', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_RUNNING))
       const client = new Client('http://m', 'test-auth-token')
-      client.getStatus('test-id')
+      return client.getStatus('test-id')
         .then(status => {
           expect(status.jobId).toEqual('test-id')
           expect(status.status).toEqual(STATUS_RUNNING)
           expect(status.result).toEqual(null)
-          done()
         })
-        .catch(done)
     })
 
-    it('properly deserializes successful job', (done) => {
+    it('properly deserializes successful job', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_SUCCESS))
       const client = new Client('http://m', 'test-auth-token')
-      client.getStatus('test-id')
+      return client.getStatus('test-id')
         .then(status => {
           expect(status.jobId).toEqual('test-id')
           expect(status.status).toEqual(STATUS_SUCCESS)
           expect(status.result.dataId).toEqual('test-data-id')
-          done()
         })
-        .catch(done)
     })
 
-    it('properly deserializes failed job', (done) => {
+    it('properly deserializes failed job', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_ERROR))
       const client = new Client('http://m', 'test-auth-token')
-      client.getStatus('test-id')
+      return client.getStatus('test-id')
         .then(status => {
           expect(status.jobId).toEqual('test-id')
           expect(status.status).toEqual(STATUS_ERROR)
           expect(status.result.dataId).toNotExist()
-          done()
         })
-        .catch(done)
     })
 
-    it('properly handles non-existent job', (done) => {
+    it('properly handles non-existent job', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_NOT_FOUND))
       const client = new Client('http://m', 'test-auth-token')
-      client.getStatus('test-id')
-        .then(() => done(new Error('Should have thrown')))
-        .catch(error => {
-          expect(error).toBeAn(Error)
-          expect(error.message).toMatch(/^InvalidResponse: Job Not Found/i)
-          done()
-        })
-        .catch(done)
+      return client.getStatus('test-id')
+        .then(
+          () => { throw new Error('Should have thrown') },
+          error => {
+            expect(error).toBeAn(Error)
+            expect(error.message).toMatch(/^InvalidResponse: Job Not Found/i)
+          })
     })
 
-    it('handles HTTP errors gracefully', (done) => {
+    it('handles HTTP errors gracefully', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(ERROR_GENERIC, 500))
       const client = new Client('http://m', 'test-auth-token')
       client.getStatus('test-id')
-        .then(() => done(new Error('Should have thrown')))
-        .catch(error => {
-          expect(error.status).toEqual(500)
-          done()
-        })
-        .catch(done)
+        .then(
+          () => { throw new Error('Should have thrown') },
+          error => {
+            expect(error.status).toEqual(500)
+          })
     })
   })
 
   describe('post()', () => {
-    it('calls correct URL', (done) => {
+    it('calls correct URL', () => {
       const stub = expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_CREATED))
       const client = new Client('http://m', 'test-auth-token')
       client.post('test-type', 'test-data')
         .then(() => {
           expect(stub.calls[0].arguments[0]).toEqual('http://m/job')
-          done()
         })
-        .catch(done)
     })
 
-    it('returns new job ID', (done) => {
+    it('returns new job ID', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_CREATED))
       const client = new Client('http://m', 'test-auth-token')
       client.post('test-type', 'test-data')
         .then(id => {
           expect(id).toEqual('test-id')
-          done()
         })
-        .catch(done)
     })
 
-    it('properly serializes message', (done) => {
+    it('properly serializes message', () => {
       const stub = expect.spyOn(window, 'fetch').andReturn(resolve(RESPONSE_JOB_CREATED))
       const client = new Client('http://m', 'test-auth-token')
       client.post('test-type', 'test-data')
@@ -364,21 +322,18 @@ describe('Piazza Client', function () {
           expect(options.method).toEqual('POST')
           expect(options.headers['content-type']).toEqual('application/json')
           expect(options.body).toEqual('{"type":"test-type","data":"test-data"}')
-          done()
         })
-        .catch(done)
     })
 
-    it('handles HTTP errors gracefully', (done) => {
+    it('handles HTTP errors gracefully', () => {
       expect.spyOn(window, 'fetch').andReturn(resolve(ERROR_GENERIC, 500))
       const client = new Client('http://m', 'test-auth-token')
       client.post('test-type', 'test-data')
-        .then(() => done(new Error('Should have thrown')))
-        .catch(error => {
-          expect(error.status).toEqual(500)
-          done()
-        })
-        .catch(done)
+        .then(
+          () => { throw new Error('Should have thrown') },
+          error => {
+            expect(error.status).toEqual(500)
+          })
     })
   })
 })
