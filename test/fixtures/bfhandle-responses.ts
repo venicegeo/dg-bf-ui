813cd1d18d886fc93dbb67d52629794461022894
David Bazile <dbazile@radiantblue.com>
2016-09-08 10:15:27 -0400

    Add bfhandle test fixtures

---
+++ test/fixtures/bfhandle-responses.ts
@@ -0,0 +1,39 @@
+export const RESPONSE_EXECUTION_SUCCESS = `{
+  "error": "",
+  "rgbLoc": "",
+  "shoreDeplID": "test-deployment-id",
+  "shoreDataID": "test-dataid",
+  "algorithmUrl": "test-algorithm-url",
+  "dateCreated": "2016-08-11T00:00:00Z",
+  "name": "test-name",
+  "sceneCaptureDate": "2016-06-08T02:05:09.126217+00:00",
+  "sceneId": "test-scene-id",
+  "sensorName": "test-sensor-name",
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          109.069921702207,
+          -0.403505719609733
+        ],
+        [
+          110.704177203293,
+          -0.752228370509914
+        ],
+        [
+          110.335043714439,
+          -2.49456676517758
+        ],
+        [
+          108.700349884252,
+          -2.14097485721046
+        ],
+        [
+          109.069921702207,
+          -0.403505719609733
+        ]
+      ]
+    ]
+  }
+}`
