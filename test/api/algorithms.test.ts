c4fbc3813af098e8f5c4638bbfd949cdcbbe908a
David Bazile <dbazile@radiantblue.com>
2016-09-16 12:59:07 -0400

    Add test stubs

---
+++ test/api/algorithms.test.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2016, RadiantBlue Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+describe('Algorithms Service', () => {
+  describe('discover()', () => {
+    it('looks for service by name')
+    it('extracts service ID')
+    it('extracts URL')
+    it('extracts interface')
+    it('extracts description')
+    it('extracts image requirements')
+    it('normalizes algorithm names')
+    it('normalizes image requirements')
+    it('throws if not logged in')
+    it('handles client errors gracefully')
+  })
+})
---
+++ test/api/productLines.test.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2016, RadiantBlue Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+describe('ProductLines Service', () => {
+  describe('create()', () => {
+    it('calls correct URL')
+    it('sends correct payload')
+    it('throws if required response data is missing')
+  })
+  describe('fetchJobs()', () => {
+    it('calls correct URL')
+    it('sends correct payload')
+    it('yields list of jobs')
+  })
+  describe('fetchProductLines()', () => {
+    it('calls correct URL')
+    it('sends correct payload')
+    it('yields list of product lines')
+    it('normalizes status')
+    it('normalizes geometry')
+    it('normalizes spatial filter name')
+    it('normalizes start and stop date')
+    it('normalizes algorithm name')
+    it('normalizes WMS layer ID')
+  })
+})
