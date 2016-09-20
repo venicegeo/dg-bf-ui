1ec45278abc8afa75ae20534833bb3d14f0a52d9
David Bazile <davidbazile@gmail.com>
2016-09-20 16:23:22 -0400

    Temporary: Cut default center back to 110W for QA

--- src/components/PrimaryMap.tsx
+++ src/components/PrimaryMap.tsx
@@ -45,7 +45,7 @@ import {
   TYPE_JOB,
 } from '../constants'
 
-const DEFAULT_CENTER = [-10, 0]
+const DEFAULT_CENTER = [110, 0]
 const MIN_ZOOM = 2.5
 const MAX_ZOOM = 22
 const RESOLUTION_CLOSE = 1000
