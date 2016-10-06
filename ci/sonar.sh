70dd70ab57eaae44609e14b286632055e8a899b4
David Bazile <dbazile@radiantblue.com>
2016-10-06 17:57:29 -0400

    CI: Fix typo and add documentation to Sonar script

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -25,12 +25,16 @@ fi
 # Note: This is a workaround for the fact that SonarQube currently doesn't support
 #       Typescript.  It can be removed once(if?) they release an official plugin
 #       for it.
+#
+#       It should also be noted that this method precludes the ability to view
+#       git blame for a file since the transpiled sources are generated on the
+#       fly.
 
 # Generate artifacts Sonar can actually parse
 ./node_modules/.bin/tsc
 
 # Update the coverage report to point to the transpiled sources
-cp report/coverage/lcov.info report/coverage/lcov.info
+cp report/coverage/lcov.info report/coverage/lcov.info~
 sed -E 's/\.tsx?$/.js/' report/coverage/lcov.info~ > report/coverage/lcov.info
 
 # HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
