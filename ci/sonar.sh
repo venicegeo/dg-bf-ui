fe4fa6d82fa9e71893a336ba3fff144b8deb59ee
David Bazile <dbazile@radiantblue.com>
2016-10-06 17:51:18 -0400

    CI: Provide correct line mappings to SonarQube (workaround)

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -17,3 +17,21 @@ if [ $(uname) == Darwin ]
   then npm run test:ci           # Local development
   else xvfb-run npm run test:ci  # Jenkins
 fi
+
+
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+
+# Note: This is a workaround for the fact that SonarQube currently doesn't support
+#       Typescript.  It can be removed once(if?) they release an official plugin
+#       for it.
+
+# Generate artifacts Sonar can actually parse
+./node_modules/.bin/tsc
+
+# Update the coverage report to point to the transpiled sources
+cp report/coverage/lcov.info report/coverage/lcov.info
+sed -E 's/\.tsx?$/.js/' report/coverage/lcov.info~ > report/coverage/lcov.info
+
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
