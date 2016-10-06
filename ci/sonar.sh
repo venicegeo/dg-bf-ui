2154fd2b90531afba071e9b67a7dd9302e09d800
David Bazile <dbazile@radiantblue.com>
2016-10-06 14:45:05 -0400

    CI: Leave _some_ artifacts for SonarQube to find

--- ci/sonar.sh
+++ ci/sonar.sh
@@ -17,3 +17,7 @@ if [ $(uname) == Darwin ]
   then npm run test:ci           # Local development
   else xvfb-run npm run test:ci  # Jenkins
 fi
+
+# EXPERIMENT/HACK EXPERIMENT/HACK EXPERIMENT/HACK EXPERIMENT/HACK EXPERIMENT/HACK
+# Leave _some_ artifact for SonarQube to find
+./node_modules/.bin/tsc
