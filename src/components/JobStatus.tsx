ae83e30faf4aff12ef28c2a959bfd29eaba7ac30
David Bazile <dbazile@radiantblue.com>
2016-12-21 21:38:32 -0500

    Fix scene ID normalizers

--- src/components/JobStatus.tsx
+++ src/components/JobStatus.tsx
@@ -220,7 +220,7 @@ export class JobStatus extends React.Component<Props, State> {
 //
 
 function normalizeSceneId(sceneId) {
-  return sceneId.replace(/^landsat:/, '')
+  return sceneId.replace(/^(landsat|rapideye|planetscope):/, '')
 }
 
 function segmentIfNeeded(s: string) {
